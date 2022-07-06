39d13dd803bb1bcf56105e7c0e7006fdecb4abf5
yoviefp <yovie@aksaramaya.com>
2022-07-06 10:59:54 +0700

    fix formating wa number

--- pages/generates.tsx
+++ pages/generates.tsx
@@ -72,15 +72,23 @@ const AdminPage = () => {
                 htmlType="submit"
                 type="primary"
                 onClick={() => {
+                  const waNumber = form.getFieldValue("wa");
+                  const waNumberReplaced =
+                    waNumber[0] !== "6"
+                      ? waNumber
+                          .replace(waNumber[0], waNumber[0] === "0" ? "62" : "")
+                          .replace(/-/g, "")
+                      : waNumber.replace(/-/g, "");
+
                   const urls =
                     url +
                     "?to=" +
                     encodeURIComponent(form.getFieldValue("name"));
                   const greetings: string = `Bismillahirrahmanirrahim\nAssalamu'alaikum Warahmatullahi wabarakatuh\n\nTanpa mengurangi rasa hormat, melalui undangan ini kami memohon doa restu sekaligus mengundang Bapak/Ibu/Saudara/i untuk menghadiri Resepsi pernikahan kami yang Insya Allah akan dilaksanakan pada hari :\nMinggu, 24 Juli 2022\n\nBersama dengan ini kami sertakan undangan pernikahan kami secara virtual.\nLink undangan:\n\n${urls}
-                \nMerupakan suatu kehormatan dan kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i berkenan hadir dengan mengikuti protokol kesehatan pencegahan penyebaran Covid-19.\nAtas perhatiannya, kami mengucapkan terima kasih.\n\nWassalamu'alaikum Warahmatullahi Wabarakatuh\n\nSalam,\nYovie & Brigita`;
-                  const urlShare = `https://api.whatsapp.com/send?phone=${form.getFieldValue(
-                    "wa"
-                  )}&text=${encodeURIComponent(greetings)}`;
+                  \nMerupakan suatu kehormatan dan kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i berkenan hadir dengan mengikuti protokol kesehatan pencegahan penyebaran Covid-19.\nAtas perhatiannya, kami mengucapkan terima kasih.\n\nWassalamu'alaikum Warahmatullahi Wabarakatuh\n\nSalam,\nYovie & Brigita`;
+                  const urlShare = `https://api.whatsapp.com/send?phone=${waNumberReplaced}&text=${encodeURIComponent(
+                    greetings
+                  )}`;
                   window?.open(urlShare, "_ blank");
                 }}
                 style={{ marginRight: 10, marginBottom: 10, width: "100%" }}
