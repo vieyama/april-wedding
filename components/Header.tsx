91909224e71b6c0bb07e81f9a8073bb7e2d508b0
yoviefp <yoviefp@gmail.com>
2022-09-03 14:06:10 +0700

    feat: april wedding

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import { MailOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 import { useRouter } from "next/router";
 import React, { useRef } from "react";
+import { wording } from "constants/wording";
 
 const HeaderComponent = ({ setPlayMusic, playMusic = false }: any) => {
   const headerRef: any = useRef();
@@ -16,30 +17,35 @@ const HeaderComponent = ({ setPlayMusic, playMusic = false }: any) => {
   return (
     <div className="header-section" ref={headerRef}>
       <div className="title-wrapper text-center" data-aos="fade-up">
-        <img src="/flower.png" alt="ss" className="flower-img" />
-        <h2 className="greeting">
-          Hello, You&apos;re Invited <br />
-          The Wedding Of
-        </h2>
-        <h1 className="brides-name">Yovie & Brigita</h1>
+        <div>
+          <img src="/flower.svg" alt="ss" className="flower-img" />
+          <h2 className="greeting">
+            Hello, You&apos;re Invited <br />
+            The Wedding Of
+          </h2>
+        </div>
+        <div>
+          <h1 className="brides-name">{wording.brideName}</h1>
 
-        <h4 className="date">24 Juli 2022</h4>
+          <h4 className="date">{wording.marriedDate}</h4>
+        </div>
+        <div>
+          <h4 className="to">
+            KEPADA YTH <br /> BAPAK/IBU/SAUDARA/I <br />
+          </h4>
 
-        <h4 className="to">
-          KEPADA YTH <br /> BAPAK/IBU/SAUDARA/I <br />
-        </h4>
+          <h4 className="person">{router?.query?.to || ""}</h4>
 
-        <h4 className="person">{router?.query?.to || ""}</h4>
-
-        <Button
-          type="primary"
-          icon={<MailOutlined />}
-          size="large"
-          shape="round"
-          onClick={handleClick}
-        >
-          Buka Undangan
-        </Button>
+          <Button
+            type="primary"
+            icon={<MailOutlined />}
+            size="large"
+            shape="round"
+            onClick={handleClick}
+          >
+            Buka Undangan
+          </Button>
+        </div>
       </div>
     </div>
   );
